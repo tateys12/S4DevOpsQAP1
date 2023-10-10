415b170dcf91eec1a19adec681f5b4d264d1c3d6
tateys12 <116300308+tateys12@users.noreply.github.com>
2023-10-10 09:46:07 -0230

    Finished

--- src/test/java/SuggestionEngineTest.java
+++ src/test/java/SuggestionEngineTest.java
@@ -17,22 +17,36 @@ public class SuggestionEngineTest {
     public void testCorrectWord(){
         suggestionengine.getWordSuggestionDB().put("apple", 1);
         suggestionengine.getWordSuggestionDB().put("banana", 1);
-        suggestionengine.getWordSuggestionDB().put("vehicle", 1);
+        suggestionengine.getWordSuggestionDB().put("ambulance", 1);
+        suggestionengine.getWordSuggestionDB().put("sleep", 1);
 
         String suggestions1 = suggestionengine.generateSuggestions("apple");
         String suggestions2 = suggestionengine.generateSuggestions("banana");
-        String suggestions3 = suggestionengine.generateSuggestions("vehicle");
+        String suggestions3 = suggestionengine.generateSuggestions("ambulance");
+        String suggestions4 = suggestionengine.generateSuggestions("sleep");
 
         assertEquals("", suggestions1);
         assertEquals("", suggestions2);
         assertEquals("", suggestions3);
+        assertEquals("", suggestions4);
     }
 @Test
     public void testSuggestCorrections() throws IOException{
-        Path dictionaryPath = Paths.get("src/main/words.txt");
+        Path dictionaryPath = Paths.get("src/main/resources/words.txt");
         suggestionengine.loadDictionaryData(dictionaryPath);
-        assertEquals("apple\nample\nalle\nale\nape\nmaple\nsaple\nable\nacle\nagle", suggestionengine.generateSuggestions("aple"));
-        assertEquals("banana\nbandana\nbanna\nbandanna\nanana\nbanaba\nbananas\nbannat\nmanana\ntanana", suggestionengine.generateSuggestions("bannana"));
-        assertEquals("vachil\nvachill\nvexil\narchil\nbechic\ncecil\nchil\ndecil\nechis\nkechel", suggestionengine.generateSuggestions("vechil"));
+
+        String suggestions1 = suggestionengine.generateSuggestions("aple");
+        System.out.println("Suggestions for 'aple':\n" + suggestions1);
+
+        String suggestions2 = suggestionengine.generateSuggestions("bannana");
+        System.out.println("Suggestions for 'bannana':\n" + suggestions2);
+
+        String suggestions3 = suggestionengine.generateSuggestions("ambulence");
+        System.out.println("Suggestions for 'ambulence':\n" + suggestions3);
+
+        assertEquals("apple\nample\nalle\nale\nape\nmaple\nsaple\nable\nacle\nagle", suggestions1);
+        assertEquals("banana\nbandana\nbanna\nbandanna\nanana\nbanaba\nbananas\nbannat\nmanana\ntanana", suggestions2);
+        assertEquals("ambulance\nambience\nambulanced\nambulancer\nambulances\nambulante", suggestions3);
+        
     }
 }
